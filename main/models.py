72c5057cfa62fa3f9230aaff8d47a7aa195df313
charlie83cl <82404370+charlie83cl@users.noreply.github.com>
2021-10-05 20:52:51 -0300

    nueva actualizacion estetica

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    autor=models.CharField(max_length=55)
+    title=models.CharField(max_length=70)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
